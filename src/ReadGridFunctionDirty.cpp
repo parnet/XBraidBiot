ce007408956f95a11d4dafe5505682b5341dc6a1
parnet <parnet@stud.uni-frankfurt.de>
2021-09-20 13:13:22 +0200

    init

---
+++ src/ReadGridFunctionDirty.cpp
@@ -0,0 +1,5 @@
+//
+// Created by parnet on 19.09.21.
+//
+
+#include "ReadGridFunctionDirty.h"
---
+++ src/ReadGridFunctionDirty.h
@@ -0,0 +1,59 @@
+//
+// Created by parnet on 19.09.21.
+//
+
+#ifndef UG_PLUGIN_XBRAIDBIOT_READGRIDFUNCTIONDIRTY_H
+#define UG_PLUGIN_XBRAIDBIOT_READGRIDFUNCTIONDIRTY_H
+
+#include <fstream>
+
+#include "lib_disc/function_spaces/grid_function.h"
+
+template<typename TDomain, typename TAlgebra>
+class GridFunctionIO {
+    typedef ug::GridFunction<TDomain, TAlgebra> TGridFunction;
+    typedef SmartPtr<TGridFunction> SPGridFunction;
+
+    typedef typename TAlgebra::vector_type::value_type TVectorValueType;
+
+    GridFunctionIO() = default;
+
+    ~GridFunctionIO() = default;
+
+    std::vector<number> times;
+
+    void write(SPGridFunction u, const char * path){
+        TGridFunction *u_ref = u->get();
+        std::ofstream outfile;
+        size_t szVector = u_ref->size();;
+
+        outfile.open(path, std::ios::binary | std::ios::out);
+        outfile.write(&szVector, sizeof(size_t));
+        // write the value for each gridpoint
+        for (size_t i = 0; i < szVector; i++) {
+            outfile.write(&(*u_ref)[i], sizeof(TVectorValueType));
+        }
+        outfile.close();
+    }
+
+    void read(SPGridFunction u, const char * path){
+        TGridFunction *u_ref = u->get();
+        std::ifstream infile;
+
+        size_t szVector = 0;
+        infile.open(path, std::ios::binary | std::ios::in);
+        // read number of gridpoints todo consistency check?
+        infile.read(&szVector, sizeof(size_t));
+
+        // read the values for each gridpoint
+        for (size_t i = 0; i < szVector; i++) {
+            TVectorValueType val = 0;
+            infile.read(&val, sizeof(TVectorValueType));
+            (*u_ref)[i] = val;
+        }
+        infile.close();
+    }
+};
+
+
+#endif //UG_PLUGIN_XBRAIDBIOT_READGRIDFUNCTIONDIRTY_H
